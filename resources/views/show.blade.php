7b79e7feed7aa4b4abec2be6c0474ae7a040a41e
Anwar Barakat <brkatanwar0@gmail.com>
2022-10-19 20:03:39 +0300

    working on movie details page part(2)

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,7 +3,8 @@
 @section('content')
     <div class="movie-info border-b border-gray-800">
         <div class="container mx-auto px-4 py-16 flex flex-col md:flex-row">
-            <img src="{{ 'https://image.tmdb.org/t/p/w500/' . $movie['poster_path'] }}" alt="" class="w-64 lg:w-96">
+            <img src="{{ 'https://image.tmdb.org/t/p/w300/' . $movie['poster_path'] }}" alt="{{ $movie['title'] }}"
+                class="w-64 lg:w-96">
             <div class="md:ml-24 md:mt-0 mt-6">
                 <h1 class="text-2xl font-semibold">{{ $movie['title'] }}</h1>
                 <div class="flex items-center text-gray-400 text-sm mt-1 flex-wrap">
@@ -32,10 +33,10 @@
                     <h4 class="text-white font-semibold">Featured Cast</h4>
                     <div class="flex gap-4 mt-4 flex-col md:flex-row">
                         @php
-                            $arrayCrews = $movie['credits']['crew'];
-                            $crews = array_map(function ($i) use ($arrayCrews) {
-                                return $arrayCrews[$i];
-                            }, array_rand($arrayCrews, 2));
+                            $crewsArray = $movie['credits']['crew'];
+                            $crews = array_map(function ($i) use ($crewsArray) {
+                                return $crewsArray[$i];
+                            }, array_rand($crewsArray, 2));
                         @endphp
                         @foreach ($crews as $crew)
                             <div>
@@ -45,18 +46,21 @@
                         @endforeach
                     </div>
                 </div>
-                <div class="mt-6">
-                    <button
-                        class="flex items-center gap-2 bg-orange-500 text-gray-900 rounded font-semibold p-4 hover:bg-orange-700 transition ease-in-out duration-150 ">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z" />
-                        </svg>
-                        Play Movie
-                    </button>
-                </div>
+                @if (count($movie['videos']['results']) > 0)
+                    <div class="mt-6">
+                        <a href="https://youtube.com/watch?v={{ $movie['videos']['results'][0]['key'] }}"
+                            class="inline-flex items-center gap-2 w-fit  bg-orange-500 text-gray-900 rounded font-semibold p-4 hover:bg-orange-700 transition ease-in-out duration-150 ">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z" />
+                            </svg>
+                            Play Movie
+                        </a>
+                    </div>
+                @endif
             </div>
         </div>
     </div>
@@ -65,33 +69,51 @@
         <div class="container mx-auto px-4 py-16">
             <h2 class="text-2xl font-semibold">Cast</h2>
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-16">
-                <div class="mt-8">
-                    <a href="#" class="">
-                        <img src="{{ asset('images/movies/education.png') }}" alt=""
-                            class="hover:opacity-75 transition ease-in-out">
-                    </a>
-                    <div>
-                        <a href="" class="text-xl mt-2 hover:text-gray-300">Real Name</a>
+                @php
+                    $castsArray = $movie['credits']['cast'];
+                    $casts = array_map(function ($i) use ($castsArray) {
+                        return $castsArray[$i];
+                    }, array_rand($castsArray, 10));
+                @endphp
+                @forelse ($casts as $cast)
+                    <div class="mt-8">
+                        <a href="#" class="">
+                            <img src="{{ 'https://image.tmdb.org/t/p/w300/' . $cast['profile_path'] }}" alt=""
+                                class="hover:opacity-75 transition ease-in-out">
+                        </a>
+                        <div>
+                            <a href="" class="text-xl mt-2 hover:text-gray-300">{{ $cast['name'] }}</a>
+                        </div>
+                        <div>
+                            <a href=""
+                                class="text-lg mt-2 hover:text-gray-300 text-gray-500">{{ $cast['character'] }}</a>
+                        </div>
                     </div>
-                    <div>
-                        <a href="" class="text-lg mt-2 hover:text-gray-300 text-gray-500">Anwar</a>
-                    </div>
-                </div>
+                @empty
+                @endforelse
 
             </div>
         </div>
     </div>
-    <div class="movie-cast border-b border-gray-800">
-        <div class="container mx-auto px-4 py-16">
-            <h2 class="text-2xl font-semibold">Images</h2>
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16">
-                <div class="mt-8">
-                    <a href="#" class="">
-                        <img src="{{ asset('images/movies/education.png') }}" alt=""
-                            class="hover:opacity-75 transition ease-in-out">
-                    </a>
+    @php
+        $images = $movie['images']['backdrops'];
+    @endphp
+    @if (count($images))
+        <div class="movie-cast border-b border-gray-800">
+            <div class="container mx-auto px-4 py-16">
+                <h2 class="text-2xl font-semibold">Images</h2>
+                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16">
+                    @forelse ($images as $image)
+                        <div class="mt-8">
+                            <a href="#" class="">
+                                <img src="{{ 'https://image.tmdb.org/t/p/w500/' . $image['file_path'] }}" alt=""
+                                    class="hover:opacity-75 transition ease-in-out">
+                            </a>
+                        </div>
+                    @empty
+                    @endforelse
                 </div>
             </div>
         </div>
-    </div>
+    @endif
 @endsection
