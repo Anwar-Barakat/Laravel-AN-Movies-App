925bb1ce217c68a4574d4fb307c3083399584185
Anwar Barakat <brkatanwar0@gmail.com>
2022-10-19 21:22:09 +0300

    add tailwind spinner when loading movies

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,13 +1,7 @@
 <div class="relative">
     <input type="text" class="bg-gray-800 rounded-full md:w-64 w-48 px-4 py-1 focus:outline-none"
         placeholder="Search..." wire:model.debounce.350ms="search">
-    <div class="absolute top-0 right-0">
-        <svg class="fill-current text-gray-500 w-4 mt-2 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none"
-            viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round"
-                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-        </svg>
-    </div>
+    <div wire:loading class="spinner  top-0 right-0 mt-4 mr-3"> </div>
     <div class="absolute bg-gray-800 rounded drop-shadow-md text-sm w-64 mt-2 overflow-hidden">
         @if (count($searchResults) > 0)
             <ul>
